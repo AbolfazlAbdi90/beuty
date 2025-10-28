6126fb00b0fa3894f1d058a7d271c906d2453f58
Abolfazl Abdi <abdiabolfazl@gmail.com>
2025-10-28 04:32:10 -0700

    Fix TypeScript any issue in discount products fo typescriptr  page

--- src/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  darkMode: 'class', // حالت تاریک با کلاس
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
-  ],
-  theme: { extend: {} },
-  plugins: [],
-}
