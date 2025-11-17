a3cc59446b6f270c20e1a0490b62c48c35bf3457
Abolfazl Abdi <abdiabolfazl@gmail.com>
2025-11-17 00:42:01 -0800

    i create and solve some propble that begong to cart components

--- src/app/Header/header.tsx
+++ src/app/Header/header.tsx
@@ -11,38 +11,51 @@ import { UseCartContext } from "../Context/CartContext";
 
 export default function Header() {
   const { handleTotalyQty } = UseCartContext();
+
   return (
     <Container>
-      {/* موبایل */}
+      {/* ------------------ موبایل ------------------ */}
       <div className="md:hidden flex flex-col py-4">
         <div className="flex items-center justify-between w-full px-4">
-          {/* Hamburger سمت چپ */}
+          {/* منو همبرگری سمت چپ */}
           <Navbar />
 
-          {/* لوگو وسط */}
-          <div className="flex-1 flex justify-center">
+          {/* لوگو وسط + کارت کنار لوگو */}
+          <div className="flex-1 flex justify-center relative">
             <Link href="/">
               <Image
                 src="/image/logo-in-website/ChatGPT Image Sep 28, 2025, 05_03_22 AM.png"
                 alt="logo-in-website"
-                width={96}
-                height={80}
-                className="rounded-2xl"
+                width={100}
+                height={100}
+                className="rounded-2xl mr-[-70px] h-[100px] w-[190px] "
               />
+            </Link>
+
+            {/* سبد خرید در موبایل (کنار لوگو سمت چپ) */}
+                {/* تعداد سبد خرید */}
+            <span className=" bg-red-500 ml-[-120px] w-[40px] px-3.75 py-2 h-[40px] text-white  rounded-full">
+              {handleTotalyQty}
+            </span>
+            <Link
+              href="/Cart"
+              className="absolute left-[0px] top-1/2 -translate-y-1/2 bg-[#EC6880] text-white py-5 px-5 rounded-full shadow-md flex items-center justify-center"
+            >
+              <FiShoppingCart size={22} />
             </Link>
           </div>
         </div>
 
-        {/* Search زیر لوگو */}
+        {/* سرچ زیر لوگو */}
         <div className="w-full mt-4 px-4">
           <Search />
         </div>
       </div>
 
-      {/* دسکتاپ */}
+      {/* ------------------ دسکتاپ ------------------ */}
       <div className="hidden md:flex flex-col py-4">
         <div className="flex items-center justify-between w-full">
-          {/* Logo + Search سمت چپ */}
+          {/* لوگو + سرچ سمت چپ */}
           <div className="flex items-center gap-4">
             <Link href="/">
               <Image
@@ -53,23 +66,30 @@ export default function Header() {
                 className="rounded-2xl"
               />
             </Link>
+
             <Search />
           </div>
 
-          {/* Cart + Profile سمت راست */}
-          <div className="flex items-center gap-4">
-            {/* پروفایل (مخفی در موبایل) */}
+          {/* سمت راست: پروفایل + تعداد سبد خرید + آیکون سبد خرید */}
+          <div className="flex items-center gap-4 pr-4">
+            {/* پروفایل */}
             <Profile />
-            <span className="ml-[-16px] mt-[-30px] bg-red-500 text-white px-3 py-1 rounded-2xl " >{handleTotalyQty}</span>
-            <div className="flex items-center  cursor-pointer justify-center bg-[#EC6880] text-white p-4 rounded-full hover:bg-pink-600 transition">
+
+            {/* تعداد سبد خرید */}
+            <span className="ml-[-16px] mt-[-30px] bg-red-500 text-white px-3 py-1 rounded-2xl">
+              {handleTotalyQty}
+            </span>
+
+            {/* آیکون سبد خرید */}
+            <div className="flex items-center  cursor-pointer bg-[#EC6880] text-white p-4 rounded-full hover:bg-pink-600 transition">
               <Link href="/Cart">
-                <FiShoppingCart size={24}></FiShoppingCart>
+                <FiShoppingCart size={24} />
               </Link>
             </div>
           </div>
         </div>
 
-        {/* Tabs زیر */}
+        {/* NavBar پایین لوگو */}
         <div className="flex justify-start mt-4 space-x-6 px-4">
           <Navbar />
         </div>
