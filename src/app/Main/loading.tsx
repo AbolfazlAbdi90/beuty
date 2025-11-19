7dbca8a1b7526c90acc3130351eeda1f65fa71a9
Abolfazl Abdi <abdiabolfazl@gmail.com>
2025-11-19 05:08:44 -0800

    i set change style for loading ProductCategory

--- src/app/Main/loading.tsx
+++ src/app/Main/loading.tsx
@@ -4,7 +4,7 @@ export default function DotsSpinner() {
   return (
     <div className="flex items-center justify-center space-x-2 h-screen  backdrop-blur-sm">
       <div className="w-3 h-3 bg-white rounded-full animate-bounce [animation-delay:-0.3s]" />
-      <div className="w-3 h-3 bg-white rounded-full animate-bounce [animation-delay:-0.15s]" />
+      <div className="w-3 h-3 mr-2 bg-white rounded-full animate-bounce [animation-delay:-0.15s]" />
       <div className="w-3 h-3 mr-2 bg-white rounded-full animate-bounce" />
     </div>
   );
